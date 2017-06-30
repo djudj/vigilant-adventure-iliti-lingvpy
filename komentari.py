3de5dd058131011e17f6cedccad7e6c67d298d66
djudj <marianomilo1@gmail.com>
2017-06-30 13:52:45 +0200

    Add files via upload

---
+++ komentari.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+# print("Ovo je prva linija")
+print("dalje")
+
+
+""" ovo je komentar
+ovo je i dalje komentar
+ovo je i dalje i dalje komentar
+"""
+
+print("Hello world!")
+print("Ovo je druga linija")
